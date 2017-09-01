8010e31978fab04fe40fa4df6b0ab9afdc91534a
CursoNET2017 <pruebaCurso2017@hotmail.com>
2017-09-01 18:02:48 +0200

    instalados paquetes

--- GrupalNET06Servidor/GrupalNET06Servidor/Global.asax.cs
+++ GrupalNET06Servidor/GrupalNET06Servidor/Global.asax.cs
@@ -18,6 +18,7 @@ namespace GrupalNET06Servidor
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            UnityConfig.RegisterComponents();
         }
     }
 }
---
+++ GrupalNET06Servidor/GrupalNET06Servidor/TipoTarea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrupalNET06Servidor
+{
+    public class TipoTarea
+    {
+        public long Id { get; set; }
+        public string Categoria { get; set; }
+        public string Descripcion { get; set; }
+        public bool Repetitivo { get; set; }
+        public bool Silenciable { get; set; }
+        public bool Automatico { get; set; }
+    }
+}
