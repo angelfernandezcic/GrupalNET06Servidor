3ed8dcb8c397bc9f0a4d10ed9e5cb62e3df38fbb
javifieldeas2017 <javifieldeas2017@outlook.es>
2017-09-05 16:27:40 +0200

    Cambio en los campos de ejecuciones

--- GrupalNET06Servidor/GrupalNET06Servidor/Ejecucion.cs
+++ GrupalNET06Servidor/GrupalNET06Servidor/Ejecucion.cs
@@ -8,11 +8,12 @@ namespace GrupalNET06Servidor
     public class Ejecucion
     {
         public long Id { get; set; }
+        public string Nombre { get; set; }
         public bool Exito { get; set; }
-        public string Descripcion { get; set; }
+        public string Mensaje { get; set; }
         public string FechaInicio { get; set; }
         public string FechaFinal { get; set; }
         public double ConsumoMemoria { get; set; }
-        public double ConsumoRed{ get; set; }
+        public double ConsumoRed { get; set; }
     }
 }
